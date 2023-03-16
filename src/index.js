d411a98898d6b6341cf91bfaeb6c47fdc7a7f7c6
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-03-16 12:06:50 -0700

    complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks');
+  
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const taskInput = document.querySelector('#new-task-description');
+    const taskText = taskInput.value.trim();
+  
+    if (taskText.length > 0) {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskText;
+  
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'x';
+      deleteButton.addEventListener('click', (e)=> {
+        taskItem.remove();
+      });
+  
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+      taskInput.value = '';
+    }
+  });
+  
 });
+
